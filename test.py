0e8c3634ea234d0376e7851e33ea4a59970aaf20
Jonny Cash <jonathan.kaesch@gmx.de>
2025-08-01 12:52:13 +0200

    Formatting

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-import pygame
 import sys
-from modules.map_converter import image_to_array
-from pdf2image import convert_from_path
 from typing import Optional
 
-#Funktionen
+import pygame
+from pdf2image import convert_from_path
+
+from modules.map_converter import image_to_array
+
+# Funktionen
+
 
 def RunningMode():
     PLAYER.speed  # Pixel pro Frame
@@ -20,16 +23,19 @@ def RunningMode():
     if keys[pygame.K_s]:
         dy = PLAYER.speed
 
-
     # Neuer Player-Rect mit versetzter Position
-    PLAYER.rect = pygame.Rect(PLAYER.pos[0] + dx, PLAYER.pos[1] + dy, PLAYER.size, PLAYER.size)
+    PLAYER.rect = pygame.Rect(
+        PLAYER.pos[0] + dx, PLAYER.pos[1] + dy, PLAYER.size, PLAYER.size
+    )
 
     # Kollision prüfen
     collided = False
     for y, row in enumerate(game_map):
         for x, tile in enumerate(row):
             if tile == 1:  # Wand
-                wall_rect = pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+                wall_rect = pygame.Rect(
+                    x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE
+                )
                 if PLAYER.rect.colliderect(wall_rect):
                     collided = True
                     break
@@ -63,51 +69,76 @@ def RunningMode():
         GameState.CurrentEnemy = Enemy1
         GameState.mode = 2
 
-
     pygame.display.flip()
 
+
 def EndScreenMode():
-   screen.fill(BLACK)
-   pygame.display.flip()
+    screen.fill(BLACK)
+    pygame.display.flip()
+
 
 def TitleScreenMode():
-    title_screen_img = convert_from_path('assets/title_screen_img.pdf', dpi=150) #liste der seiten
-    title_screen_img = title_screen_img[0].convert("RGB") #erste seite als pdf nehmen
+    title_screen_img = convert_from_path(
+        "assets/title_screen_img.pdf", dpi=150
+    )  # liste der seiten
+    title_screen_img = title_screen_img[0].convert("RGB")  # erste seite als pdf nehmen
 
-    pg_image = pygame.image.fromstring(title_screen_img.tobytes(), title_screen_img.size, 'RGB')
-    pg_image = pygame.transform.scale(pg_image, (800, 600)) 
+    pg_image = pygame.image.fromstring(
+        title_screen_img.tobytes(), title_screen_img.size, "RGB"
+    )
+    pg_image = pygame.transform.scale(pg_image, (800, 600))
 
     screen.blit(pg_image, (0, 0))
     pygame.display.flip()
 
+
 def FightingMode(Enemy):
     screen.fill(WHITE)
     fighting_player_size = 40
-    player_rect = pygame.Rect(SCREEN_WIDTH/6, SCREEN_HEIGHT/2 - fighting_player_size, fighting_player_size, fighting_player_size)
+    player_rect = pygame.Rect(
+        SCREEN_WIDTH / 6,
+        SCREEN_HEIGHT / 2 - fighting_player_size,
+        fighting_player_size,
+        fighting_player_size,
+    )
     pygame.draw.rect(screen, BLUE, player_rect)
 
     print(Enemy.size)
 
     pygame.display.flip()
 
-
     # WIN Debug
     keys = pygame.key.get_pressed()
     if keys[pygame.K_o]:
-        # PLAYER.pos[0] += Enemy.size 
-        # PLAYER.pos[1] += Enemy.size 
+        # PLAYER.pos[0] += Enemy.size
+        # PLAYER.pos[1] += Enemy.size
         GameState.CurrentEnemy = None
         GameState.mode = 1
 
 
-#Klassen
+# Klassen
 class GameStateManager:
     def __init__(self):
         self.mode = 4
         self.CurrentEnemy: Optional[Enemy] = None
 
+
 class Player:
-    def __init__(self, pos: list, name: str, size: int, diff: float, type: str, xp: int, hp: int, speed: int, dodge: float, att: int, cp: float, defe: int):
+    def __init__(
+        self,
+        pos: list,
+        name: str,
+        size: int,
+        diff: float,
+        type: str,
+        xp: int,
+        hp: int,
+        speed: int,
+        dodge: float,
+        att: int,
+        cp: float,
+        defe: int,
+    ):
         self.name = name
         self.pos = pos
         self.size = size
@@ -120,12 +151,13 @@ class Player:
         self.att = att
         self.cp = cp
         self.defe = defe
-        
+
         self.rect = pygame.Rect(self.pos[0], self.pos[1], self.size, self.size)
 
     def engage(self, enemy):
         GameState.mode = 2
         GameState.CurrentEnemy = enemy
+
 
 class Enemy:
     def __init__(self, pos: list, id, type, size: int, att, defe, diff):
@@ -137,6 +169,7 @@ class Enemy:
         self.defe = defe * diff
 
         self.rect = pygame.Rect(self.pos[0], self.pos[1], self.size, self.size)
+
 
 # Init
 pygame.init()
@@ -163,16 +196,18 @@ game_map = array
 
 # Spieler starten
 player_pos = [TILE_SIZE * 2, TILE_SIZE * 2]  # Startposition
-speed = 60 # FPS
+speed = 60  # FPS
 
 # PLAYER Init
-PLAYER = Player([10, 10], "Player1", TILE_SIZE - 1, 1.0, "melee", 0, 100, 2, 0.1, 10, 0.1, 20)
+PLAYER = Player(
+    [10, 10], "Player1", TILE_SIZE - 1, 1.0, "melee", 0, 100, 2, 0.1, 10, 0.1, 20
+)
 
 # Debug Init
 GlobalDevEnemy = Enemy([10, 10], 0, "melee", 20, 10, 40, 1.0)
 
 
-#Clock
+# Clock
 clock = pygame.time.Clock()
 game = True
 
@@ -183,7 +218,6 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-
 
     if GameState.mode == 1:
         # RunningMode for gameplay
@@ -223,15 +257,15 @@ while game:
             GameState.mode = 4
 
     if GameState.mode == 4:
-            TitleScreenMode()
+        TitleScreenMode()
 
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_1]:
-                GameState.mode = 1
-            if keys[pygame.K_2]:
-                GameState.mode = 2
-            if keys[pygame.K_3]:
-                GameState.mode = 3
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_1]:
+            GameState.mode = 1
+        if keys[pygame.K_2]:
+            GameState.mode = 2
+        if keys[pygame.K_3]:
+            GameState.mode = 3
 
 
 # Beenden
